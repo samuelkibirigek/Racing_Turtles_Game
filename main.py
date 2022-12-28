37c2e17258ff86759060ecae9b87884673ceda03
Kibirige Kalule Samuel <samuelkibirigek@gmail.com>
2022-12-28 20:43:14 +0300

    Turtle race initial commit.

---
+++ main.py
@@ -0,0 +1,38 @@
+from turtle import Turtle, Screen
+import random
+
+screen = Screen()
+screen.setup(width=500, height=400)
+
+user_choice = screen.textinput(title="You have a variety of colored turtle.", prompt="What turtle do you bet on?")
+print(user_choice)
+turtles = []
+
+
+def setup(turtle_object, shade, y):
+    turtle_object = Turtle(shape="turtle")
+    turtle_object.color(shade)
+    turtle_object.penup()
+    turtle_object.goto(-230, y)
+    turtles.append(turtle_object)
+
+
+setup(turtle_object="tim", shade="blue", y=0)
+setup(turtle_object="jim",shade="grey", y=60)
+setup(turtle_object="tom", shade="red", y=120)
+setup(turtle_object="sam", shade="orange", y=-60)
+setup(turtle_object="kim", shade="purple", y=-120)
+
+race_is_on = True
+while race_is_on:
+    for turtle in turtles:
+        if turtle.xcor() < 230:
+            turtle.forward(random.randint(1, 10))
+        else:
+            race_is_on = False
+            if turtle.pencolor() == user_choice.lower():
+                print(f"You win, the fastest turtle was {turtle.pencolor()}")
+            elif turtle.pencolor() != user_choice.lower():
+                print(f"You lose, the fastest turtle was {turtle.pencolor()}")
+
+screen.exitonclick()
